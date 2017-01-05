bc7be2c21be6435e6484ae750dba42ea83305624
Arda Ozkal <ardaozkal@live.com>
2017-01-05 17:25:01 +0300

    Fixed yet another bug

--- easyvpn.sh
+++ easyvpn.sh
@@ -20,7 +20,7 @@ then
     . vars
     ./pkitool $client
     ../client-configs/make_config.sh $client
-    curl -s --user $mailgunapikey https://api.mailgun.net/v3/$mailgundomainname/messages -F from=$mailgunfrom -F to=$mail -F subject=$mailgunsubject -F text=$mailguncontents -F attachment=@../client-configs/files/$client.ovpn
+    curl -s --user "$mailgun_apikey" https://api.mailgun.net/v3/$mailgun_domainname/messages -F from="$mailgun_from" -F to="$mail" -F subject="$mailgun_subject" -F text="$mailgun_contents" --form-string html="$mailgun_htmlcontents" -F h:Reply-To="$mailgun_replyto"
     echo "" #mailgun takes a line ugh
     echo "Completed and sent to $mail."
 else
