d4259f01c81a5a0bdc4f1a5993462b7b70c191d5
Arda Ozkal <ardaozkal@live.com>
2017-01-05 15:20:47 +0300

    Updated to make it easier to configurate

--- easyvpn.sh
+++ easyvpn.sh
@@ -2,8 +2,14 @@
 
 client=$1
 mail=$2
+vpnname='SomeVPN'
+mailgunapikey='api:insert_mailgun_apikey_here' #has to start with api:
+mailgundomainname='example.com'
+mailgunfrom='SomeVPN Invitation <somemail@yourdomain.com>' #has to contain the actual mail in brackets
+mailgunsubject='You are invited to SomeVPN'
+mailguncontents='Hello there!\nYou are invited to trying SomeVPN!\nYou can view installation steps on https://blog.ardao.me/connecting-to-ardavpn\n\nMerhabalar, SomeVPNi denemeye davetlisiniz!\nKurmak icin adimlari https://blog.ardao.me/ardavpne-baglanmak adresinde bulabilirsiniz.'
 
 . vars
 ./pkitool $client
 ../client-configs/make_config.sh $client
-curl -s --user 'api:PUTYOURMAILGUNAPIKEYHERE' https://api.mailgun.net/v3/PUTYOURDOMAINHER/messages -F from='ArdaVPN Registration <ardavpn@mg.ardao.me>' -F to=$mail -F subject='ArdaVPN Invitation' -F text='Hello there! You are invited to try ArdaVPN! You can view installation steps on https://blog.ardao.me/connecting-to-ardavpn/ ( https://blog.ardao.me/ardavpne-baglanmak/ for turkish instructions)' -F attachment=@../client-configs/files/$client.ovpn
+curl -s --user $mailgunapikey https://api.mailgun.net/v3/$mailgundomainname/messages -F from=$mailgunfrom -F to=$mail -F subject=$mailgunsubject-F text=$mailguncontents -F attachment=@../client-configs/files/$client.ovpn
